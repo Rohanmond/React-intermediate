b96510cc0af663bee324865fe0bcd82af5162f9b
Rohan Mondal <mondalrohan108@gmail.com>
2023-03-05 14:15:50 +0530

    Added typescript

--- Class4/src/entryBrowser.tsx
+++ Class4/src/entryBrowser.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 // passing a react element inside the root
 root.render(<App />);
