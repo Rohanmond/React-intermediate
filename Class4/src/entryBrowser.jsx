f74c55fc65146f624677b856c8db9959ace43dc8
Rohan Mondal <rohan.mondal@razorpay.com>
2023-03-05 10:30:09 +0530

    Added class 4

---
+++ Class4/src/entryBrowser.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// passing a react element inside the root
+root.render(<App />);
