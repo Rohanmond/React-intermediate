065fcd8ed171ab7ff0f24bc00fe9061da11d0bf6
Rohan Mondal <mondalrohan108@gmail.com>
2023-03-08 23:38:47 +0530

    Added jest

---
+++ Testing/src/entryBrowser.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+// passing a react element inside the root
+root.render(<App />);
